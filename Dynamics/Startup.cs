0d3c870ddbfcb0a34f8589e353742363b2889f9f
21704004 <21704004@dut4life.ac.za>
2019-10-09 02:29:48 +0200

    changes

---
+++ Dynamics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dynamics.Startup))]
+namespace Dynamics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
